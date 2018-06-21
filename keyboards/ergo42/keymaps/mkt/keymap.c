63c37aff10a7d3b253ab5247a03c2b867b6f627f
Makoto Ito <ito.makoto@interlink-j.com>
2018-06-21 16:53:38 +0900

    fix typo.

--- keyboards/ergo42/keymaps/mkt/keymap.c
+++ keyboards/ergo42/keymaps/mkt/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_DELT, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
     KC_TAB,   KC_GRV,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  \
     MO(SYMB), KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_RSFT, \
-    MO(META), KC_LCTL, KC_LGUI,KC_PSCE, KC_INS,  KC_LALT, KC_SPC, KC_SPC, KC_MINS, KC_EQL,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
+    MO(META), KC_LCTL, KC_LGUI,KC_PSCR, KC_INS,  KC_LALT, KC_SPC, KC_SPC, KC_MINS, KC_EQL,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 
   /* META
