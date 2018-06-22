cc1f0e4db5cc78551048bdba69e493fe3e7badae
Makoto Ito <ito.makoto@interlink-j.com>
2018-06-22 14:16:08 +0900

    fix single quot.

--- keyboards/ergo42/keymaps/mkt/keymap.c
+++ keyboards/ergo42/keymaps/mkt/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, KC_EXLM, S(KC_2), KC_HASH, KC_DLR,  KC_PERC, KC_RBRC, KC_BSLS, JP_AMPR,    JP_CIRC,    XXXXXXX, KC_RO,      S(KC_JYEN), _______, \
+    _______, KC_EXLM, S(KC_2), KC_HASH, KC_DLR,  KC_PERC, KC_RBRC, KC_BSLS, JP_AMPR,    S(KC_7),    XXXXXXX, KC_RO,      S(KC_JYEN), _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, JP_LPRN,    JP_RPRN,    XXXXXXX, S(KC_SCLN), S(KC_QUOT), _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_RBRC), S(KC_BSLS), XXXXXXX, JP_UNDS,    _______,    _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______,    _______,    _______  \
