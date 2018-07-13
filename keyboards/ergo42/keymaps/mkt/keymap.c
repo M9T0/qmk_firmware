a640b290f9358851a370e8a467d5139838118e82
Makoto Ito <ito.makoto@interlink-j.com>
2018-07-13 12:48:30 +0900

    数字キーの配置を調整

--- keyboards/ergo42/keymaps/mkt/keymap.c
+++ keyboards/ergo42/keymaps/mkt/keymap.c
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* META
    ,------------------------------------------------.   ,------------------------------------------------.
-   | Reset|   1  |   2  |   3  |   4  |   5  |  [   |   |  ]   |   6  |   7  |   8  |   9  |   0  | Bksp |
+   | Reset|      |   1  |   2  |   3  |   4  |  5   |   |  6   |   7  |   8  |   9  |   0  | PGUP | Bksp |
    |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   | Tab  |      |      |Muhen | Henk |      |      |   | Left | Down |  Up  |Right | PGDN | PGUP | Enter|
+   | Tab  |      |      |Muhen | Henk |      |      |   | Left | Down |  Up  |Right |      | PGDN | Enter|
    |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    | SYMB |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   |  F7  |  F8  |  F9  | F10  | F11  | F12  | RSft |
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_RBRC,    KC_BSLS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, XXXXXXX, XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_PGUP, _______, \
+    RESET,   XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP, _______, \
+    _______, XXXXXXX, XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_PGDN, _______, \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, \
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX  \
   ),
