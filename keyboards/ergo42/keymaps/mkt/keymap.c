ce4162dce29e42a6f0645d80faecc8d9bb756c14
Makoto Ito <ito.makoto@interlink-j.com>
2020-07-16 16:38:49 +0900

    レイヤー切換えにLTを使用するようにキーマップを変更

--- keyboards/ergo42/keymaps/mkt/keymap.c
+++ keyboards/ergo42/keymaps/mkt/keymap.c
@@ -19,16 +19,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |------+------+------+------+------+------+------|   |------+------+------+------+------+-------+------|
    | Tab  |  `   |   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |  ;+  |  :*   | Enter|
    |------+------+------+------+------+------+------|   |------+------+------+------+------+-------+------|
-   | SYMB | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |  ,<  |  .>  |  /?  | RSft  | SYMB |
+   | LSft |  ^~  |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |  ,<  |  .>  |  /?  |  -=   | RSft |
    |------+------+------+------+------+------+------|   |------+------+------+------+------+-------+------|
-   | META | LCtrl| LGUI | PScr | INS  | LALT |Space |   |Space |  -=  |  ^~  | HOME | End  | RCtl  | META |
+   | LCtl | LAlt | LGUI | PScr | INS  | Muhen| Space|   | Space|Henkan| APP  | HOME | End  | RAlt  | RCtl |
    `------------------------------------------------'   `-------------------------------------------------'
    */
   [BASE] = LAYOUT( \
     KC_ESC,   KC_DELT, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_BSPC, \
     KC_TAB,   KC_GRV,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
-    MO(SYMB), KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(SYMB),\
-    MO(META), KC_LCTL, KC_LGUI,KC_PSCR, KC_INS,  KC_LALT, KC_SPC,  KC_SPC, KC_MINS, KC_EQL,  KC_HOME, KC_END,  KC_RCTL, MO(META) \
+    KC_LSFT,  KC_EQL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, KC_RSFT, \
+    KC_LCTL, KC_LALT,  KC_LGUI,KC_PSCR, KC_INS,  LT(SYMB, KC_MHEN), LT(META, KC_SPC),  LT(META, KC_SPC), LT(SYMB, KC_HENK), KC_APP,  KC_HOME, KC_END,  KC_RALT,  KC_RCTL \
   ),
 
   /* META
@@ -37,16 +37,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    | Tab  |      |      |  F1  |  F2  |  F3  |  F4  |   | Left | Down |  Up  |Right |      | PGDN | Enter|
    |------+------+------|------+------+------+------+   |------+------+------+------+------+------+------|
-   | SYMB | LSft |      |  F5  |  F6  |  F7  |  F8  |   |  F9  |  F10 |  F11 | F12  |      | RSft | SYMB |
+   | LSft |      |      |  F5  |  F6  |  F7  |  F8  |   |  F9  |  F10 |  F11 | F12  |      |      | RSft |
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   | META | LCtrl| LGUI | PScr | INS  | LALT |Muhen |   | Henk |  -=  |  ^~  | HOME | End  | RCtl | META |
+   | LCtl | LAlt | LGUI | PScr | INS  | Muhen|Space |   |Space |Henkan| App  | HOME | End  | RAlt | RCtl |
    `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
     _______, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP, _______, \
     _______, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_PGDN, _______, \
-    _______, _______, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, _______, _______, \
-    _______, _______, _______, _______, _______, _______, KC_MHEN,  KC_HENK, _______, _______, _______, _______, _______, _______  \
+    _______, XXXXXXX, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, _______, \
+    _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______  \
   ),
 
   /* SYMB
@@ -55,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    | Tab  |      |      |      |      |      |      |   |  [   |  {   |      |      |  +   |  *   | Enter|
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   | SYMB | LSft |      |      |      |      |      |   |  ]   |  }   |      |      |  _   | RSft | SYMB |
+   | LSft |      |      |      |      |      |      |   |  ]   |  }   |      |      |  _   |      | RSft |
    |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   | META | LCtrl| LGUI | PScr | INS  | LALT |Space |   |Space |  -=  |  ^~  | HOME | End  | RCtl | META |
+   | LCtl | LAlt | LGUI | PScr | INS  | Muhen| Space|   | Space|Henkan| App  | HOME | End  | RAlt | RCtl |
    `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
     _______, XXXXXXX, KC_EXLM, S(KC_2), KC_HASH, KC_DLR,  KC_PERC,  JP_AMPR, S(KC_7),    JP_LPRN, JP_RPRN, KC_RO,      S(KC_JYEN), _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_RBRC, S(KC_RBRC), XXXXXXX, XXXXXXX, S(KC_SCLN), S(KC_QUOT), _______, \
-    _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_BSLS, S(KC_BSLS), XXXXXXX, XXXXXXX, JP_UNDS,    _______,    _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_BSLS, S(KC_BSLS), XXXXXXX, XXXXXXX, JP_UNDS,    XXXXXXX,    _______, \
     _______, _______, _______, _______, _______, _______, _______,  _______, _______,    _______, _______, _______,    _______,    _______  \
   )
 
